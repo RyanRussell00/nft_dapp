bd0baf8d14ccabc321d50449b5099fd41a189d8e
Ryan Russell <ryan.russell2000@gmail.com>
2021-05-24 07:05:33 -0700

    Metamask now prompts for contract deployment, but it costs 45 ETH

--- src/components/App.js
+++ src/components/App.js
@@ -1,79 +1,131 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import Web3 from "web3";
+// Import the ABI of the contract
 import Color from "../abis/Color.json";
 
 class App extends Component {
-
     async componentDidMount() {
         await this.loadWeb3();
-        await this.loadBlockchain();
+        await this.loadAccounts();
+        //        await this.loadBlockchain();
+        await this.deployContractAsUser();
     }
 
+    // Prompts Metamask to open and ask for user to connect application with MetaMask
     async loadWeb3() {
         if (window.ethereum) {
-            window.web3 = new Web3(window.ethereum)
+            window.web3 = new Web3(window.ethereum);
             await window.ethereum.enable();
         } else if (window.web3) {
-            window.web3 = new Web3(window.web3.currentProvider)
+            window.web3 = new Web3(window.web3.currentProvider);
         } else {
             window.alert("Non-Ethereum Browser. Please install Metamask");
         }
     }
 
-    async loadBlockchain() {
-        const web3 = window.web3
+    async loadAccounts() {
+        const web3 = window.web3;
         // Load account
-        const accounts = await web3.eth.getAccounts()
-        this.setState({account: accounts[0]})
+        const accounts = await web3.eth.getAccounts();
+        this.setState({ account: accounts[0] });
+    }
 
-        const networkId = await web3.eth.net.getId()
+    // Built this function following SO answer: https://ethereum.stackexchange.com/a/11097
+    async deployContractAsUser() {
+        const web3 = window.web3;
+        // let contractFile = JSON.parse("../abis/Color.json");
+
+        let byteCodeOfContract = Color["bytecode"];
+
+        let SampleContract = new web3.eth.Contract(Color.abi);
+
+        console.log("Current account: " + this.state.account);
+
+        SampleContract.deploy({
+            data: byteCodeOfContract,
+        })
+            .send(
+                {
+                    from: this.state.account,
+                    gas: 1500000,
+                    gasPrice: "30000000000000",
+                },
+                function(error, transactionHash) {
+                    console.log("I got here");
+                }
+            )
+            .on("error", function(error) {
+                console.log("ERROR", error);
+            })
+            .on("transactionHash", function(transactionHash) {
+                console.log("TRANSACTION HASH", transactionHash);
+            })
+            .on("receipt", function(receipt) {
+                console.log(receipt.contractAddress); // contains the new contract address
+            })
+            .on("confirmation", function(confirmationNumber, receipt) {
+                console.log(
+                    "CONF NUMBER AND RECEIPT",
+                    confirmationNumber,
+                    receipt
+                );
+            })
+            .then(function(newContractInstance) {
+                console.log(newContractInstance.options.address); // instance with the new contract address
+            });
+    }
+
+    async loadBlockchain() {
+        const web3 = window.web3;
+
+        const networkId = await web3.eth.net.getId();
         console.log("NetworkId: ", networkId);
-        const networkData = Color.networks[networkId]
+        const networkData = Color.networks[networkId];
         if (networkData) {
-            const abi = Color.abi
-            const address = networkData.address
-            const contract = new web3.eth.Contract(abi, address)
-            this.setState({contract})
-            const totalSupply = await contract.methods.totalSupply().call()
-            this.setState({totalSupply})
+            const abi = Color.abi;
+            const address = networkData.address;
+            const contract = new web3.eth.Contract(abi, address);
+            this.setState({ contract });
+            const totalSupply = await contract.methods.totalSupply().call();
+            this.setState({ totalSupply });
             // Load Colors
             for (var i = 1; i <= totalSupply; i++) {
-                const color = await contract.methods.colors(i - 1).call()
+                const color = await contract.methods.colors(i - 1).call();
                 this.setState({
-                    colors: [...this.state.colors, color]
-                })
+                    colors: [...this.state.colors, color],
+                });
             }
         } else {
-            window.alert('Smart contract not deployed to detected network.')
+            window.alert("Smart contract not deployed to detected network.");
         }
     }
 
     constructor(props) {
         super(props);
         this.state = {
-            account: '',
+            account: "",
             contract: null,
             totalSupply: 0,
-            colors: []
-        }
+            colors: [],
+        };
     }
 
     mint = (color) => {
         // Use send() to send transaction to blockchain
-        this.state.contract.methods.mint(color).send(
-            {from: this.state.account}
-        )
-            .once('receipt', (receipt) => {
+        this.state.contract.methods
+            .mint(color)
+            .send({ from: this.state.account })
+            .once("receipt", (receipt) => {
                 this.setState({
-                    colors: [this.state.colors, color]
+                    colors: [this.state.colors, color],
                 });
             });
-    }
+    };
 
     async addExistingToken(tokenId) {
-        console.log("Started ownerOf: ", tokenId)
-        const owner = await this.state.contract.methods.ownerOf(tokenId).call()
+        console.log("Started ownerOf: ", tokenId);
+        const owner = await this.state.contract.methods.ownerOf(tokenId).call();
         console.log("owner: ", owner);
         // this.state.contract.methods.ownerOf(tokenId).send(
         //     {from: this.state.account}
@@ -82,9 +134,7 @@ class App extends Component {
         // })
     }
 
-    transfer = (to, tokenId) => {
-
-    }
+    transfer = (to, tokenId) => {};
 
     render() {
         return (
@@ -94,69 +144,76 @@ class App extends Component {
                         className="navbar-brand col-sm-3 col-md-2 mr-0"
                         href="http://www.dappuniversity.com/bootcamp"
                         target="_blank"
-                        rel="noopener noreferrer"
-                    >
+                        rel="noopener noreferrer">
                         Color Tokens
                     </a>
-                    <p className="text-white px-3 py-1">
-                        {this.state.account}
-                    </p>
+                    <p className="text-white px-3 py-1">{this.state.account}</p>
                 </nav>
                 <div className="container-fluid mt-5">
                     <div className="row">
-                        <main role="main" className="col-lg-12 d-flex text-center">
+                        <main
+                            role="main"
+                            className="col-lg-12 d-flex text-center">
                             <div className="content mr-auto ml-auto">
                                 <h1>New Token</h1>
-                                <form onSubmit={(event) => {
-                                    event.preventDefault();
-                                    const color = this.color.value;
-                                    this.mint(color);
-                                }}>
+                                <form
+                                    onSubmit={(event) => {
+                                        event.preventDefault();
+                                        const color = this.color.value;
+                                        this.mint(color);
+                                    }}>
                                     <input
                                         type="text"
                                         className="form-control mb-1"
                                         placeholder="e.g. #FFFFF"
                                         ref={(input) => {
-                                            this.color = input
-                                        }}/>
+                                            this.color = input;
+                                        }}
+                                    />
                                     <input
                                         type="submit"
                                         className="btn btn-primary btn-block"
-                                        value="MINT"/>
+                                        value="MINT"
+                                    />
                                 </form>
-                                <hr/>
-                                <form onSubmit={(event) => {
-                                    event.preventDefault();
-                                    const token = this.inputTokenId.value;
-                                    this.addExistingToken(token);
-                                }}>
+                                <hr />
+                                <form
+                                    onSubmit={(event) => {
+                                        event.preventDefault();
+                                        const token = this.inputTokenId.value;
+                                        this.addExistingToken(token);
+                                    }}>
                                     <input
                                         type="text"
                                         className="form-control mb-1"
                                         placeholder="Enter Existing Token ID"
                                         ref={(input) => {
-                                            this.inputTokenId = input
-                                        }}/>
+                                            this.inputTokenId = input;
+                                        }}
+                                    />
                                     <input
                                         type="submit"
                                         className="btn btn-primary btn-block"
-                                        value="Add Existing Token"/>
+                                        value="Add Existing Token"
+                                    />
                                 </form>
                             </div>
                         </main>
                     </div>
-                    <hr/>
+                    <hr />
                     <div className="row text-center">
                         {this.state.colors.map((color, key) => {
-                            return (<>
-                                    <div className="color m-2" key={key}
-                                         style={{backgroundColor: color}}>
-                                    </div>
-                                    <div>
-                                        {color}
-                                    </div>
+                            return (
+                                <>
+                                    <div
+                                        className="color m-2"
+                                        key={key}
+                                        style={{
+                                            backgroundColor: color,
+                                        }}></div>
+                                    <div>{color}</div>
                                 </>
-                            )
+                            );
                         })}
                     </div>
                 </div>
